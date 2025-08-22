7c43e842e7c101f1ad7c5be260db4241abcd6fc3
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-08-22 09:48:50 +0900

    [Feat] Player 상태 스크립트들 추가
    - Player의 각 상태를 표현할 상태 스크립트들 추가.
    - PlayerRuntimeData를 받아서 각종 처리를 진행할 예정.
    - 일단 내부 구현이 되지 않은 각 상태 클래스도 추가.

---
+++ Assets/000WorkSpaces/JTW/Scripts/Player/PlayerStates/PlayerState_Idle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState_Idle : PlayerStateBase
+{
+    public PlayerState_Idle(StateMachine<PlayerStates> stateMachine, PlayerRunTimeData data) : base(stateMachine, data)
+    {
+    }
+
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void Update()
+    {
+    }
+}
---
+++ Assets/000WorkSpaces/JTW/Scripts/Player/PlayerStates/PlayerState_Work.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState_Work : PlayerStateBase
+{
+    public PlayerState_Work(StateMachine<PlayerStates> stateMachine, PlayerRunTimeData data) : base(stateMachine, data)
+    {
+    }
+
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void Update()
+    {
+    }
+}
