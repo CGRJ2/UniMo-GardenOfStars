59abde3ed4e59ddfb6b797edc5f6c46fbc22288e
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-08-27 14:25:18 +0900

    [Feat] 스테이지 리스트 UI패널 프리펩 추가 / 상호작용 시 지역 이동 패널(스테이지 리스트)을 활성화 시켜주는 건물: 포탈 추가

--- Assets/000WorkSpaces/CJM/Scripts/Interactables/BuildingSystem/BuildingActivePopUI.cs
+++ Assets/000WorkSpaces/CJM/Scripts/Interactables/BuildingSystem/BuildingActivePopUI.cs
@@ -21,6 +21,10 @@ public class BuildingActivePopUI : MonoBehaviour
         {
             btn_Info.onClick.AddListener(OpenInfoPanel_Harvest);
         }
+        else if (buildingInstance is Portal)
+        {
+            btn_Info.onClick.AddListener(OpenStageListPanel);
+        }
     }
     public void OpenInfoPanel_Harvest()
     {
@@ -93,6 +97,38 @@ public class BuildingActivePopUI : MonoBehaviour
         });
     }
 
+    public void OpenStageListPanel()
+    {
+        if (UIManager.Instance == null)
+        {
+            Debug.LogError("[건물 정보 패널] UIManager.Instance가 null입니다!");
+            return;
+        }
+
+        // 이미 TitlePanel이 열려있는지 확인
+        var existingPanels = Manager.ui.GetUIsByLayer(UILayerType.Panel);
+        foreach (var panel in existingPanels)
+        {
+            if (panel is StageListPanel)
+            {
+                //Debug.Log("[HUDAllPanel] 이미 TitlePanel이 열려있습니다. 중복 호출 무시");
+                return;
+            }
+        }
+
+        // 업그레이드 패널 열기
+        Manager.ui.ShowPanelAsync<StageListPanel>((panel) =>
+        {
+            if (panel != null)
+            {
+                //Debug.Log("[HUDAllPanel] TitlePanel 성공적으로 열림");
+            }
+            else
+            {
+                //Debug.LogError("[HUDAllPanel]  열기 실패");
+            }
+        });
+    }
     
 
     public void ActiveUpgradeBtnView()
