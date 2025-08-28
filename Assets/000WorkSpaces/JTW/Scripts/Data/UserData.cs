31e9826b307355c65be5992eeec50429e42d8368
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-08-28 17:43:35 +0900

    [Refactor] UserData를 partial 클래스로 변경
    - 여러 사람이 건들이는 클래스이므로 partial 클래스로 변경.

--- Assets/000WorkSpaces/JTW/Scripts/Data/UserData.cs
+++ Assets/000WorkSpaces/JTW/Scripts/Data/UserData.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class UserData : FirebaseData
+public partial class UserData : FirebaseData
 {
     public FirebaseDataList<WorkerData> WorkerList;
 
     public UserData(string id, string parentPath = null) : base(id, parentPath)
     {
-
         WorkerList = new FirebaseDataList<WorkerData>("WorkerList", Path, (id, parentPath) =>
         {
             return new WorkerData(id, parentPath);
