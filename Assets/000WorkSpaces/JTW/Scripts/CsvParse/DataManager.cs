1339f8f81ea489e6ce41d3d55c02774ddf6bf657
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-08-29 16:31:42 +0900

    [Feat] DataManager 싱글톤 클래스 추가
    - Csv 데이터 초기화를 진행하고 들고 있을 DataManager 싱글톤 클래스 추가.
    - 데이터를 파싱하는 방식의 대한 정의를 각자 해야하기 때문에, partial 클래스로 정의하여 각자 파일에서 진행할 수 있도록 함.
    - Addressable 번들을 사용하는 방식과 구글 드라이브 등에서 다운로드해서 사용하는 방식 2가지를 자유롭게 번갈아서 사용할 수 있도록 bool 변수로 2개 중에 선택할 수 있도록 함.

---
+++ Assets/000WorkSpaces/JTW/Scripts/CsvParse/DataManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public partial class DataManager : Singleton<DataManager>
+{
+    private void Awake()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+    }
+
+    private async Task<string> GetDataString(bool isAdressable)
+    {
+        if (isAdressable)
+        {
+            AsyncOperationHandle<TextAsset> handle = Addressables.LoadAssetAsync<TextAsset>(_workerAdress);
+            try
+            {
+                TextAsset asset = await handle.Task;
+
+                return asset.text;
+            }
+            catch
+            {
+                Debug.Log("[DataManager] Addressable 루틴 실패");
+                return null;
+            }
+            finally
+            {
+                Addressables.Release(handle);
+            }
+        }
+        else
+        {
+            var tcs = new TaskCompletionSource<UnityWebRequestAsyncOperation>();
+
+            UnityWebRequest request = UnityWebRequest.Get(_workerDataTableURL);
+            UnityWebRequestAsyncOperation operation = request.SendWebRequest();
+
+            operation.completed += _ => tcs.SetResult(operation);
+
+            await tcs.Task;
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log($"[DataManager] 다운로드 루틴 실패 : {request.error}");
+                return null;
+            }
+
+            return request.downloadHandler.text;
+        }
+    }
+}
