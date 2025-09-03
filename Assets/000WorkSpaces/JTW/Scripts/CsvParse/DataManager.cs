8c8d4380e17296bd603707baef68a0229537ff35
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-03 16:16:43 +0900

    [Feat] WorkerCsv 데이터 연결
    - 이전에 누락되었던 Lv당 스탯과 각종 랭크 등을 추가.
    - DataManager에 CharacterLv 초기화 로직 추가.

--- Assets/000WorkSpaces/JTW/Scripts/CsvParse/DataManager.cs
+++ Assets/000WorkSpaces/JTW/Scripts/CsvParse/DataManager.cs
@@ -14,6 +14,7 @@ public partial class DataManager : Singleton<DataManager>
     public void Init()
     {
         WorkerRoutine();
+        CharacterLvRoutine();
     }
 
     private async Task<string> GetDataString(bool isAdressable, string address)
