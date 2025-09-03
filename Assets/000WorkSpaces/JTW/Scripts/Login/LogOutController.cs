e104d24dff00abb901b1b0c5b1fdb40de1909a26
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-03 12:02:58 +0900

    [Feat] LogOutController 클래스 추가
    - 게스트 계정 로그아웃을 담당하는 LogOutController 클래스 추가.

---
+++ Assets/000WorkSpaces/JTW/Scripts/Login/LogOutController.cs
@@ -0,0 +1,18 @@
+﻿using Firebase.Auth;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogOutController : MonoBehaviour
+{
+    private FirebaseAuth Auth => Manager.firebase.Auth;
+
+    public void LogOut()
+    {
+        // 익명만 로그아웃 가능
+        if (!Auth.CurrentUser.IsAnonymous) return;
+
+        Auth.SignOut();
+        // TODO : 로그인 화면으로
+    }
+}
