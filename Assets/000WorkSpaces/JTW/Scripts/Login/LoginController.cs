206bfc0d12d13dce3a9ca0bcd84e6f52c95ae5bd
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-03 12:01:58 +0900

    [Feat] LoginController 클래스 추가
    - 게스트 로그인, PlayGames로그인 기능이 있는 LoginController 클래스 추가.
    - Login 씬에 추가하여 로그인을 진행하거나, 로그인이 완료되었음을 알리는 역할을 할 예정.

---
+++ Assets/000WorkSpaces/JTW/Scripts/Login/LoginController.cs
@@ -0,0 +1,83 @@
+﻿using Firebase.Auth;
+using Firebase.Extensions;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class LoginController : MonoBehaviour
+{
+    public ObservableProperty<bool> IsLogined = new();
+
+    private FirebaseAuth Auth => Manager.firebase.Auth;
+
+    public void PlayGameLogin()
+    {
+        PlayGamesPlatform.Instance.ManuallyAuthenticate(OnMaunuallyAuthenticate);
+    }
+
+    private void OnMaunuallyAuthenticate(SignInStatus status)
+    {
+        if (status != SignInStatus.Success)
+        {
+            Debug.Log($"PlayGames 로그인 실패 : {status}");
+            return;
+        }
+
+        Debug.Log("PlayGame로그인 성공");
+
+        PlayGamesToFirebase();
+    }
+
+    // TODO : Firebase 서버의 문제로 중단되면 그에 맞는 처리 추가.
+    private void PlayGamesToFirebase()
+    {
+        if (Auth.CurrentUser != null)
+        {
+            Auth.SignOut();
+        }
+
+        PlayGamesPlatform.Instance.RequestServerSideAccess(false, authCode =>
+        {
+            Credential credential = PlayGamesAuthProvider.GetCredential(authCode);
+
+            Manager.firebase.Auth.SignInWithCredentialAsync(credential).ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCanceled)
+                {
+                    Debug.Log("파이어베이스 연동 중단");
+                    return;
+                }
+
+                if (task.IsFaulted)
+                {
+                    Debug.Log($"파이어베이스 연동 실패 : {task.Exception}");
+                    return;
+                }
+
+                Debug.Log("파이어베이스 연동 성공");
+                IsLogined.Value = true;
+            });
+        });
+    }
+
+    public void GusetLogin()
+    {
+        Manager.firebase.Auth.SignInAnonymouslyAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("익명 로그인 중단");
+                return;
+            }
+
+            if (task.IsFaulted)
+            {
+                Debug.Log($"익명 로그인 실패 : {task.Exception}");
+                return;
+            }
+
+            Debug.Log("익명 로그인 성공");
+            IsLogined.Value = true;
+        });
+    }
+}
