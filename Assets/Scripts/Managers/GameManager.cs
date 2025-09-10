e815f19717670e90fbd030e0c680404a1d9a39c7
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-10 22:08:47 +0900

    [Fix] StageDataCsv 파싱 과정에 주소 유효성 검사 추가
    - try catch를 해도 경고 로그가 뜨기 때문에, 주소 유효성 검사를 하는 방식으로 변경.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceLocations;
-using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -222,7 +221,7 @@ public partial class DataManager
             stage.Name_KR = words[dict["Name_Korean"]];
             stage.Name_En = words[dict["Name_English"]];
 
-            try
+            if (Addressables.ResourceLocators.Any(locator => locator.Locate($"Sprite/{words[dict["WheelSprite"]]}", typeof(Sprite), out var locations)))
             {
                 Addressables.LoadAssetAsync<Sprite>($"Sprite/{words[dict["WheelSprite"]]}").Completed += task =>
                 {
@@ -235,12 +234,8 @@ public partial class DataManager
                     stage.WheelSprite = task.Result;
                 };
             }
-            catch (InvalidKeyException e)
-            {
-                Debug.LogWarning(e.Message);
-            }
 
-            try
+            if (Addressables.ResourceLocators.Any(locator => locator.Locate($"Sprite/{words[dict["CenterSprite"]]}", typeof(Sprite), out var locations)))
             {
                 Addressables.LoadAssetAsync<Sprite>($"Sprite/{words[dict["CenterSprite"]]}").Completed += task =>
                 {
@@ -253,11 +248,6 @@ public partial class DataManager
                     stage.WheelSprite = task.Result;
                 };
             }
-            catch (InvalidKeyException e)
-            {
-                Debug.LogWarning(e.Message);
-            }
-
 
             int.TryParse(words[dict["RequiredQuestIndex"]], out stage.RequiredQuestIndex);
 
