7e5fb8a8b36ceab84746cc2a22331c6f620874eb
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-10 18:08:13 +0900

    [Feat] FirebaseDataList에 Count 추가.
    - FirebaseDataList에 Count 추가.

--- Assets/000WorkSpaces/JTW/Scripts/Firebase/FirebaseDataList.cs
+++ Assets/000WorkSpaces/JTW/Scripts/Firebase/FirebaseDataList.cs
@@ -15,6 +15,8 @@ public class FirebaseDataList<T> : FirebaseData where T : FirebaseData
 
     public UnityEvent<T> OnAdded = new();
 
+    public int Count => _list.Count;
+
     public FirebaseDataList(string id, string parentPath, Func<string, string, T> factory) : base(id, parentPath)
     {
         _factory = factory;
