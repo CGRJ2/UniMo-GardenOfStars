eba7084a22c31a1369f780817c15479752310491
coffeelatt <ssongcoffee@gmail.com>
2025-09-11 18:19:26 +0900

    [Fix] 편의성추가
    테스트용 인스펙터창에서 조정하는 옵션변경

--- Assets/000WorkSpaces/SHY/Script/testads.cs
+++ Assets/000WorkSpaces/SHY/Script/testads.cs
@@ -1,21 +1,53 @@
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+
+
 
 public class testads : MonoBehaviour
 {
+    [Header("광고 제거 상태")]
+    public bool adRemoved = false;
     
-    // Start is called before the first frame update
-    void Start()
+
+
+
+
+    IEnumerator Start()
     {
-        
+        // 저장된 광고 제거 상태 불러오기
+        yield return new WaitForSeconds(2f); // UI 조정이 끝난 후 실행되도록 대기
+        ApplyBannerState();
+       
+
     }
+    
+    public void ApplyBannerState()
+    {
+        if (adRemoved ==true)
+        {
+            Manager.ad.HideBannerAd();
+            Debug.Log("광고 제거 상태 적용됨");
+        }
+        
+        else if(GameObject.Find($"{Manager.ad.bannerSize}(Clone)") ==null)
+        {
+            Manager.ad.LoadBannerAd();
+            Debug.Log("광고 표시 상태 적용됨");
+        }
+        else
+        {
+            Debug.Log("광고가 이미 제거되어있거나 표시중입니다.");
+        }
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void Mainclick()
     {
@@ -27,6 +59,7 @@ public class testads : MonoBehaviour
     {
         Manager.ad.ShowRewardedAd(() =>
         {
+            //Manager.player.Data.Money.Value += 333;
             FindAnyObjectByType<TestUi>()?.Reward();
         });
         //FindAnyObjectByType<AdManager>()?.ShowRewardedAd(() =>
