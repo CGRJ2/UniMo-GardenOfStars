61eb6f65e3c106b1c0622c936393392e40ec37f2
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-11 12:20:41 +0900

    [Feat] Character Csv 데이터 추가
    - 캐릭터의 이름과 설명에 대한 데이터를 가진 Csv데이터 추가.
    - Addressable에 외형과 관련된 에셋 추가.(에셋 자체는 비공개)

---
+++ Assets/000WorkSpaces/JTW/Scripts/Charactor/Worker/CsvData/CharacterDataCsv.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CharacterDataCsv : IUsableId
+{
+    public string Id;
+
+    public string Name_Kr;
+    public string Name_En;
+    public string Description;
+
+    public string GetId()
+    {
+        return Id;
+    }
+}
+
+public partial class DataManager
+{
+    [SerializeField] private bool _isCharacterAdressable = true;
+
+    // 구글 스프레드 시트 다운로드 주소
+    private const string _characterDataTableURL = "https://docs.google.com/spreadsheets/d/1CwrcyyODjYAwjCgYkofKQl815o-vOWkUH7yy6mdUtY4/export?format=csv&gid=0";
+
+    // Addressable 에셋 주소
+    private const string _characterAdress = "CharacterCsv";
+
+    public DataTableParser<CharacterDataCsv> CharacterCost;
+    private async void CharacterRoutine()
+    {
+        string dataCsv;
+
+        if (_isCharacterAdressable)
+        {
+            dataCsv = await GetDataString(_isCharacterAdressable, _characterAdress);
+        }
+        else
+        {
+            dataCsv = await GetDataString(_isCharacterAdressable, _characterDataTableURL);
+        }
+
+        CharacterCost = new DataTableParser<CharacterDataCsv>((words, dict) =>
+        {
+            CharacterDataCsv character = new CharacterDataCsv();
+
+            character.Id = words[dict["CharacterID"]];
+
+            character.Name_Kr = words[dict["Name_Kr"]];
+            character.Description = words[dict["Desc"]];
+
+            return character;
+        });
+
+        CharacterCost.Load(dataCsv);
+    }
+}
