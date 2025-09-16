ac3afba3a4b252332b98d533af76b2cbb720aff7
최재민 <80324256+CGRJ2@users.noreply.github.com>
2025-09-16 10:18:00 +0900

    [Fix] 튜토리얼 스테이지의 부동산 패널에 빈 슬롯이 추가 생성되던 오류 수정
    - 건물 정보 슬롯 중복 생성 방지 추가

--- Assets/000WorkSpaces/KYS/Scripts/UI/UIPanel/PropertyPanel.cs
+++ Assets/000WorkSpaces/KYS/Scripts/UI/UIPanel/PropertyPanel.cs
@@ -64,21 +64,28 @@ namespace KYS
                 Addressables.LoadAssetAsync<BuildingData>(TutorialManager.Instance.tutoHarvestBuildingID).Completed += task =>
                 {
                     BuildingData bd = task.Result;
-                    PropertyContent content = Instantiate(contentPrefab, contentParent).GetComponent<PropertyContent>();
-                    contentInstances.Add(bd.ID, content);
 
-                    // PropertyContent 초기화
-                    content.Initialize();
+                    // 건물 정보 슬롯 생성 (중복 생성 방지)
+                    if (!contentInstances.ContainsKey(bd.ID))
+                    {
+                        PropertyContent content = Instantiate(contentPrefab, contentParent).GetComponent<PropertyContent>();
+                        contentInstances.Add(bd.ID, content);
 
-                    // 업그레이드 정보가 있는 건물이라면 해당 정보도 같이 업데이트
-                    Dictionary<string, UpgradeData> upgradeDic = Manager.buildings.upgradeDataDic;
-                    if (upgradeDic.ContainsKey(bd.ID)) // 현재 건물에 업그레이드 정보가 있다면
-                    {
-                        content.SetBuildingData(bd, upgradeDic[bd.ID]);
-                    }
-                    else
-                    {
-                        content.SetBuildingData(bd);
+                        Debug.LogError($"{bd.ID} 슬롯 생성");
+
+                        // PropertyContent 초기화
+                        content.Initialize();
+
+                        // 업그레이드 정보가 있는 건물이라면 해당 정보도 같이 업데이트
+                        Dictionary<string, UpgradeData> upgradeDic = Manager.buildings.upgradeDataDic;
+                        if (upgradeDic.ContainsKey(bd.ID)) // 현재 건물에 업그레이드 정보가 있다면
+                        {
+                            content.SetBuildingData(bd, upgradeDic[bd.ID]);
+                        }
+                        else
+                        {
+                            content.SetBuildingData(bd);
+                        }
                     }
                 };
 
