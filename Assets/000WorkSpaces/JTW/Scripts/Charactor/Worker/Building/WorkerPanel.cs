bb571f85dd1b3f73fe14abe6387f613fad5f266e
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-18 09:21:02 +0900

    [Feat] 일꾼이 잠금 상태일 때, 이름이 보이지 않도록 수정
    - 일꾼이 잠금 상태일 때, 이름을 ???로 뜨도록 수정.

--- Assets/000WorkSpaces/JTW/Scripts/Charactor/Worker/Building/WorkerPanel.cs
+++ Assets/000WorkSpaces/JTW/Scripts/Charactor/Worker/Building/WorkerPanel.cs
@@ -145,7 +145,10 @@ public class WorkerPanel : KYS.BaseUI
     public void SetInfo(WorkerPanelStates state)
     {
         _state = state;
-        
+
+        string key = $"{_workerKey}_{Manager.firebase.UserData.CurStage.Value}";
+        CharacterDataCsv chracterData = Manager.data.Character.Values[key];
+
         // 모든 패널 비활성화
         if (_upgradeButton != null) _upgradeButton.SetActive(false);
         if (_beforeHireScreen != null) _beforeHireScreen.SetActive(false);
@@ -155,14 +158,17 @@ public class WorkerPanel : KYS.BaseUI
         switch (state)
         {
             case WorkerPanelStates.Upgrade:
+                _runWorkerNameText.text = chracterData.GetName();
                 if (_upgradeButton != null) _upgradeButton.SetActive(true);
                 break;
                 
             case WorkerPanelStates.Purchase:
+                _runWorkerNameText.text = chracterData.GetName();
                 if (_beforeHireScreen != null) _beforeHireScreen.SetActive(true);
                 break;
                 
             case WorkerPanelStates.Locked:
+                _runWorkerNameText.text = "???";
                 if (_lockScreen != null) _lockScreen.SetActive(true);
                 break;
         }
