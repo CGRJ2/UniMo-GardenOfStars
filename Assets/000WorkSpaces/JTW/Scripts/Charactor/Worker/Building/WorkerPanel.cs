f1b5f9cd0eb0b467676b6ace910f2c66dee6ef6c
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-18 17:25:49 +0900

    [Feat] WorkerPanel에 QuestOrder 프로퍼티 추가.
    - WorkerPanel에서 QusetOrder 데이터를 쉽게 확인할 수 있게 하기위해 프로퍼티 추가.
    - 해금 조건 string에 활용될 예정.

--- Assets/000WorkSpaces/JTW/Scripts/Charactor/Worker/Building/WorkerPanel.cs
+++ Assets/000WorkSpaces/JTW/Scripts/Charactor/Worker/Building/WorkerPanel.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -38,6 +37,8 @@ public class WorkerPanel : KYS.BaseUI
 
     private int _employCost;
     private int _employBMCost;
+
+    private int QuestOrder => Manager.data.WorkerEmployCost.Values[$"{_workerKey}_{Manager.firebase.UserData.CurStage.Value}"].QuestOrder;
 
     private bool _isBMCost;
 
