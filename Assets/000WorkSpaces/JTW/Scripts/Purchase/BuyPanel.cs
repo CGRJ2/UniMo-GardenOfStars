d4ae9eb8d482605cedcaf3722e1e79286029c625
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-22 11:22:43 +0900

    [Fix] ShopPanel의 레이아웃이 제대로 보이지 않던 현상 수정.
    - 동적으로 생성된 요소가 레이아웃에 제대로 들어가지 않는 현상 수정.
    - 동적으로 요소를 생성한 뒤 레이아웃을 리빌드 하는 것으로 해결.

--- Assets/000WorkSpaces/JTW/Scripts/Purchase/BuyPanel.cs
+++ Assets/000WorkSpaces/JTW/Scripts/Purchase/BuyPanel.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuyPanel : MonoBehaviour
 {
     [SerializeField] private GameObject _buyButtonPrefab;
     [SerializeField] private RewardTypes _rewardTypes;
 
+    [SerializeField] private GameObject _itemPanel;
+    [SerializeField] private GameObject _contentPanel;
     [SerializeField] private ShopPanel _shopPanel;
 
-    private void Awake()
+    private void Start()
     {
         foreach (var value in Manager.data.Buy.Values)
         {
@@ -19,6 +22,16 @@ public class BuyPanel : MonoBehaviour
 
             buy.SetInfo(value.Key, _shopPanel);
         }
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_itemPanel.GetComponent<RectTransform>());
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_contentPanel.GetComponent<RectTransform>());
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_shopPanel.GetComponent<RectTransform>());
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
     }
 }
