cb88782e3689bbb27b7098881d6e645a1d8dff99
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-09-23 11:53:24 +0900

    [Feat] 튜토리얼 시퀀스 추가
    - 건물 업그레이드, 일꾼 업그레이드, 전당포 판매, 플레이어 업그레이드 설명 추가

--- Assets/000WorkSpaces/CJM/Scripts/NpcInteractAreaUI.cs
+++ Assets/000WorkSpaces/CJM/Scripts/NpcInteractAreaUI.cs
@@ -16,6 +16,12 @@ public class NpcInteractAreaUI : MonoBehaviour
         var stageID = Manager.firebase.UserData.CurStage.Value;
         var npc = Manager.firebase.UserData.CurStageData.Npc;
         
+        if (stageID == "Tutorial")
+        {
+            Debug.LogWarning("튜토리얼 완료 대화 실행(업그레이드 패널 진입 용도)");
+            return;
+        }
+
         // 퀘스트가 클리어 상태라면
         if (npc.CurQuestData.QuestState.Value == 3)
         {
