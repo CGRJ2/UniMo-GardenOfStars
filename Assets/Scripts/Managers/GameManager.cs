dbe17e2b7eb69cefc03833e26963474ef41c59c5
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-26 19:34:11 +0900

    [Feat] 아직 출시되지 않은 스테이지는 갈 수 없도록 변경
    - 아직 출시되지 않은 스테이지는 "준비중"이라는 문구와 함께 갈 수 없도록 변경.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : Singleton<GameManager>
 
         StartCoroutine(Fetch());
     }
-    
+
     #region Addressable Assets Storage 동기화 체크
 
     public IEnumerator Fetch()
@@ -33,7 +33,7 @@ public class GameManager : Singleton<GameManager>
         var checkHandle = Addressables.CheckForCatalogUpdates(false);  // 변경된 카탈로그 ID들
         yield return checkHandle;
 
-        if(checkHandle.Status == AsyncOperationStatus.Failed)
+        if (checkHandle.Status == AsyncOperationStatus.Failed)
         {
             Manager.firebase.NetworkDisconnected();
             yield break;
@@ -47,7 +47,7 @@ public class GameManager : Singleton<GameManager>
             var updateCatalogHandle = Addressables.UpdateCatalogs(checkHandle.Result, false);
             yield return updateCatalogHandle;
 
-            if(updateCatalogHandle.Status == AsyncOperationStatus.Failed)
+            if (updateCatalogHandle.Status == AsyncOperationStatus.Failed)
             {
                 Manager.firebase.NetworkDisconnected();
                 yield break;
@@ -77,7 +77,7 @@ public class GameManager : Singleton<GameManager>
             }
             Debug.Log($"다운로드사이즈 어싱크{sizeCheckHandle.Result}");
 
-            if(sizeCheckHandle.Result <= 0)
+            if (sizeCheckHandle.Result <= 0)
             {
                 SafeRelease(ref updateCatalogHandle);
                 SafeRelease(ref checkHandle);
@@ -97,7 +97,7 @@ public class GameManager : Singleton<GameManager>
                 Debug.Log($"다운로드진행상황{downloadStatus.DownloadedBytes} / {sizeCheckHandle.Result}bytes 다운됨. 퍼센트:{(int)downloadStatus.Percent * 100}");
                 downloadProgress.Value = downloadStatus.Percent;
 
-                if(downloadHandle.Status == AsyncOperationStatus.Failed)
+                if (downloadHandle.Status == AsyncOperationStatus.Failed)
                 {
                     inDownloading = false;
                     Manager.firebase.NetworkDisconnected();
@@ -151,6 +151,8 @@ public class StageDataCsv : IUsableId
     public int StageInflationRate;
     public int StageAutoReward;
 
+    public bool IsRelease;
+
 
     public string GetId()
     {
@@ -199,37 +201,20 @@ public partial class DataManager
             stage.Name_KR = words[dict["Name_Korean"]];
             stage.Name_En = words[dict["Name_English"]];
 
-            if (Addressables.ResourceLocators.Any(locator => locator.Locate($"Sprite/{words[dict["WheelSprite"]]}", typeof(Sprite), out var locations)))
-            {
-                /*Addressables.LoadAssetAsync<Sprite>($"Sprite/{words[dict["WheelSprite"]]}").Completed += task =>
-                {
-                    if (task.Status != AsyncOperationStatus.Succeeded)
-                    {
-                        Debug.LogWarning("WheelSprite 로드 실패");
-                        return;
-                    }
-
-                    stage.WheelSprite = task.Result;
-                };*/
-            }
-
-            if (Addressables.ResourceLocators.Any(locator => locator.Locate($"Sprite/{words[dict["CenterSprite"]]}", typeof(Sprite), out var locations)))
-            {
-                /*Addressables.LoadAssetAsync<Sprite>($"Sprite/{words[dict["CenterSprite"]]}").Completed += task =>
-                {
-                    if (task.Status != AsyncOperationStatus.Succeeded)
-                    {
-                        Debug.LogWarning("CenterSprite 로드 실패");
-                        return;
-                    }
-
-                    stage.WheelSprite = task.Result;
-                };*/
-            }
-
             int.TryParse(words[dict["RequiredQuestIndex"]], out stage.RequiredQuestIndex);
             int.TryParse(words[dict["InflationRate"]], out stage.StageInflationRate);
             int.TryParse(words[dict["StageAutoReward"]], out stage.StageAutoReward);
+
+            int.TryParse(words[dict["IsRelease"]], out int isRelease);
+
+            if (isRelease == 0)
+            {
+                stage.IsRelease = false;
+            }
+            else
+            {
+                stage.IsRelease = true;
+            }
 
             stage.NextStageId = words[dict["NextStageId"]];
 
