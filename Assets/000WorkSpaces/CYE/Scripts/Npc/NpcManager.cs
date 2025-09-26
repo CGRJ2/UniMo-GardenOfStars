d0d1eb993724ea5d4a84f7a0e8a296bc78e2a1dc
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-09-26 19:20:43 +0900

    [Fix] 스테이지 클리어 판정 Index 설정 오류 수정

--- Assets/000WorkSpaces/CYE/Scripts/Npc/NpcManager.cs
+++ Assets/000WorkSpaces/CYE/Scripts/Npc/NpcManager.cs
@@ -4,6 +4,7 @@ using GameNpc;
 public class NpcManager : Singleton<NpcManager>
 {
     public NpcData CurStageNpc => Manager.firebase.UserData.CurStageData.Npc;
+    //public FirebaseProperty<string> CurNpcId => Manager.firebase.UserData.CurStageData.Npc.NpcID;
 
     private void Awake()
     {
@@ -14,4 +15,11 @@ public class NpcManager : Singleton<NpcManager>
     {
         // 초기화
     }
+
+    // 현재 스테이지의 NpcID 등록 및 반환
+    /*public string CurStageNpcDataInit(string curStageID)
+    {
+        CurNpcId.Value = Manager.data.Stage.Values[curStageID].NpcID;
+        return CurNpcId.Value;
+    }*/
 }
