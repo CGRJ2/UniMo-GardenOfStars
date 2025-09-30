1a7458f4598c3d07cddbecf5561ed875cced1566
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-09-30 09:34:14 +0900

    [Feat] 때때로 퀘스트의 순서가 이상하게 나오는 현상 수정
    - FirebaseDataList가 Add를 할 때 순서를 보장하지 않아서 생기는 문제로 추정.
    - FirebaseDataList의 List에 접근할 때 Id를 기준으로 정렬한 데이터를 주도록 변경.

--- Assets/000WorkSpaces/JTW/Scripts/Skin/Data/SkinData.cs
+++ Assets/000WorkSpaces/JTW/Scripts/Skin/Data/SkinData.cs
@@ -13,13 +13,13 @@ public class SkinData : FirebaseData
         {
             return new CharacterSkinData(id, parentPath);
         });
-        CharacterSkinList.List.Add(new CharacterSkinData("101", Path));
+        CharacterSkinList.AddListItem(new CharacterSkinData("101", Path));
 
         EquipSkinList = new FirebaseDataList<EquipSkinData>("EquipSkinList", Path, (id, parentPath) =>
         {
             return new EquipSkinData(id, parentPath);
         });
-        EquipSkinList.List.Add(new EquipSkinData("201", Path));
+        EquipSkinList.AddListItem(new EquipSkinData("201", Path));
 
         InitList.Add(CharacterSkinList);
         InitList.Add(EquipSkinList);
