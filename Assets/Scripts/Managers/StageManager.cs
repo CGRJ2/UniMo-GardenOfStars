22d925633ce923062e4fd40732623f5aacc6665b
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-10-03 20:37:25 +0900

    [Fix] 퀘스트 목록이 제대로 채워져야 진행되도록 변경
    - 퀘스트 목록이 제대로 채워져야 진행되도록 변경.

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -59,7 +59,12 @@ public class StageManager : MonoBehaviour
 
         Manager.quest.CurStageQuestDataInit();
 
-        yield return new WaitUntil(() => Manager.firebase.UserData.CurStageData.Npc.QuestList.Count >= 0);
+
+        string npcId = Manager.data.Stage.Values[Manager.firebase.UserData.CurStage.Value].NpcID;
+        var curStageQuestDatas = Manager.data.Quest.Values.Where(item => item.Value.NpcId == npcId).OrderBy(i => i.Value.QuestOrder);
+        int count = curStageQuestDatas.Count();
+
+        yield return new WaitUntil(() => Manager.firebase.UserData.CurStageData.Npc.QuestList.Count >= count);
         yield return new WaitUntil(() => Manager.firebase.UserData.CurStageData.Npc.QuestList.IsInit);
         Debug.LogWarning("QuestList Inited");
 
